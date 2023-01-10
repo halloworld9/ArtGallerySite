26f92724feea6c4bfb9b2160839dd7913005b23a
Hallworld <prishedko.ilya@yandex.ru>
2023-01-10 17:00:38 +0400

    Added controller and empty templates for pages

---
+++ src/main/java/com/photographersite/controller/AlbumController.java
@@ -0,0 +1,26 @@
+package com.photographersite.controller;
+
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AlbumController {
+    @GetMapping(value = "/albums/{title}")
+    public String getAlbum(@PathVariable String title, Model page) {
+        return "album";
+    }
+
+    @GetMapping("/albums/") //redirect from empty path variables
+    public String redirect() { //without it will be error 404
+        return "forward:/albums";
+    }
+
+    @GetMapping("/albums")
+    public String getAlbums() {
+        return "albums";
+    }
+
+}
