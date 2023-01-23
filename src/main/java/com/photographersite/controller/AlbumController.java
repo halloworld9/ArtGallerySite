a31e0f49adc3cbfa75ffb08324cbe4633e8822f4
Hallworld <prishedko.ilya@yandex.ru>
2023-01-23 19:08:04 +0400

    Added transfer list of albums

--- src/main/java/com/photographersite/controller/AlbumController.java
+++ src/main/java/com/photographersite/controller/AlbumController.java
@@ -1,12 +1,20 @@
 package com.photographersite.controller;
 
+import com.photographersite.service.AlbumService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @Controller
 public class AlbumController {
+    private final AlbumService albumService;
+
+    public AlbumController(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
     @GetMapping(value = "/albums/{title}")
     public String getAlbum(@PathVariable String title, Model page) {
         return "album";
@@ -19,8 +27,9 @@ public class AlbumController {
 
     @GetMapping("/albums")
     public String getAlbums(Model page) {
-
+        page.addAttribute("albums", albumService.getAlbums());
         return "albums";
     }
+
 
 }
