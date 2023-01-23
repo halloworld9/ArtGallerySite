21e08fdc5f1c923269ac5bc972c1d66216670a38
Hallworld <prishedko.ilya@yandex.ru>
2023-01-23 18:56:58 +0400

    Change columns names to sql style. Added manyToOne annotation for better sql architecture. And added PhotoRepository

--- src/main/java/com/photographersite/entity/Photo.java
+++ src/main/java/com/photographersite/entity/Photo.java
@@ -1,17 +1,29 @@
 package com.photographersite.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
-public class Photo {
+@Setter
+public class Photo  {
+    public Photo() {
+    }
+
+    public Photo(String title, String pathToImage) {
+        this.title = title;
+        this.pathToImage = pathToImage;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id")
     private Long id;
     private String title;
+    @Column(name = "path_to_image", nullable = false)
     private String pathToImage;
+    @ManyToOne
+    @JoinColumn(name = "album_title")
+    private Album album;
 }
---
+++ src/main/java/com/photographersite/repository/PhotoRepository.java
@@ -0,0 +1,9 @@
+package com.photographersite.repository;
+
+import com.photographersite.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
