8eb9ca9783c6589aec6e3ec21eaacb24b48b58a5
Hallworld <prishedko.ilya@yandex.ru>
2023-01-28 20:20:44 +0400

    Save album using SaverService and use default album cover - universal cover. Also save new album variable - visible.

--- src/main/java/com/photographersite/controller/AdminController.java
+++ src/main/java/com/photographersite/controller/AdminController.java
@@ -3,26 +3,24 @@ package com.photographersite.controller;
 import com.photographersite.entity.Album;
 import com.photographersite.entity.Photo;
 import com.photographersite.service.AlbumService;
-import com.photographersite.util.UploadUtil;
+import com.photographersite.service.SaverService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 @Controller
 public class AdminController {
     private final AlbumService albumService;
-
-    public AdminController(AlbumService albumService) {
+    private final SaverService saverService;
+    public AdminController(AlbumService albumService, SaverService saverService) {
         this.albumService = albumService;
+        this.saverService = saverService;
     }
 
 
@@ -31,32 +29,48 @@ public class AdminController {
         return "admin";
     }
 
-    @PostMapping("/admin")
-    public ResponseEntity<?> saveAlbum(@RequestAttribute final MultipartFile image, @RequestAttribute final MultipartFile zip
-            , @RequestParam String title, @RequestParam String description) {
-        String uploadDir = System.getProperty("user.dir") + "/albums";
+    @DeleteMapping("/admin")
+    public String deleteAlbum(@RequestParam String title) {
+        albumService.deleteAlbum(title);
+        return "admin";
+    }
 
-        File albumDir = new File(uploadDir);
-        if (!albumDir.exists())
-            albumDir.mkdir();
-        String albumPath = uploadDir + "/" + title;
-        new File(albumPath).mkdir();
-        Set<Photo> photos = UploadUtil.unzipFiles(zip, albumPath);
-        String imageType = image.getOriginalFilename();
-        imageType = imageType.substring(imageType.lastIndexOf("."));
-        String coverPath = Paths.get(albumPath + "/" + title + imageType).toString();
-        byte[] buffer = new byte[4 * 1024];
-        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(coverPath));
-             BufferedInputStream bis = new BufferedInputStream(image.getInputStream())) {
-            while (bis.read(buffer) != -1)
-                bos.write(buffer);
-
+    @PutMapping("/admin")
+    public ResponseEntity<?> saveAlbum(@RequestAttribute final MultipartFile cover, @RequestAttribute final MultipartFile[] photos
+            , @RequestParam String title, @RequestParam String description, @RequestParam Boolean visible) {
+        try {
+            var createAlbumState = saverService.saveAlbum(cover, photos, title);
+            String coverPath = "/images/universal cover.png";
+            Set<Photo> photosSet = new HashSet<>();
+            Album album = new Album();
+            switch (createAlbumState) {
+                case CREATED -> {
+                    coverPath = "/photos/" + title + "/" + title + "." + cover.getContentType().substring(6);
+                    fillPhotosSet(photos, title, photosSet, album);
+                }
+                case CREATED_WITHOUT_COVER -> fillPhotosSet(photos, title, photosSet, album);
+            }
+            album.setPhotos(photosSet)
+                    .setCoverPath(coverPath)
+                    .setTitle(title)
+                    .setDescription(description)
+                    .setVisible(visible);
+            albumService.saveAlbum(album);
         } catch (IOException e) {
             e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        Album album = new Album(title, coverPath, description, photos);
-        photos.forEach((x) -> x.setAlbum(album));
-        albumService.saveAlbum(album);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    private void fillPhotosSet(MultipartFile[] photos, String title, Set<Photo> photosSet, Album album) {
+        for (MultipartFile photo : photos) {
+            String photoTitle = photo.getOriginalFilename();
+            photoTitle = photoTitle.substring(0, photoTitle.lastIndexOf('.'));
+            String path = "/photos/" + title + "/" + photo.getOriginalFilename();
+            photosSet.add(new Photo(photoTitle, path, album));
+        }
+    }
+
+
 }
