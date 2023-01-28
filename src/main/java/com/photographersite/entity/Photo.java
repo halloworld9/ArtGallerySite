a7c9b65357e39c4a17d4b20fb25a192967d96a0b
Hallworld <prishedko.ilya@yandex.ru>
2023-01-28 20:23:25 +0400

    Change album constructor, override equals and hashCode methods.

--- src/main/java/com/photographersite/entity/Photo.java
+++ src/main/java/com/photographersite/entity/Photo.java
@@ -7,13 +7,13 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Photo  {
+public class Photo {
     public Photo() {
     }
-
-    public Photo(String title, String pathToImage) {
+    public Photo(String title, String pathToImage, Album album) {
         this.title = title;
         this.pathToImage = pathToImage;
+        this.album = album;
     }
 
     @Id
@@ -26,4 +26,28 @@ public class Photo  {
     @ManyToOne
     @JoinColumn(name = "album_title")
     private Album album;
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Photo photo = (Photo) o;
+
+        if (!id.equals(photo.id)) return false;
+        if (!title.equals(photo.title)) return false;
+        if (!pathToImage.equals(photo.pathToImage)) return false;
+        return album.getTitle().equals(photo.album.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + title.hashCode();
+        result = 31 * result + pathToImage.hashCode();
+        result = 31 * result + album.getTitle().hashCode();
+        return result;
+    }
 }
