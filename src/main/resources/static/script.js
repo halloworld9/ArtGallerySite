4405ba05f68e59835554dba47afdc1b4f4eb042e
Hallworld <prishedko.ilya@yandex.ru>
2023-01-28 20:38:58 +0400

    Update script.js for put request

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,15 +1,28 @@
 async function uploadFile() {
     let formData = new FormData();
-    formData.append("image", image.files[0]);
-    formData.append("zip", zip.files[0]);
+    formData.append("cover", cover.files[0]);
+    formData.append("visible", visible.checked);
+    formData.append("title", title.value)
+    formData.append("description", description.value)
+    let list = photos.files;
+    for (let i = 0; i < list.length; i++)
+        formData.append("photos", list[i]);
 
 
-    await fetch('admin?' + new URLSearchParams({
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value
-    }), {
-        method: "POST",
+    await fetch('admin', {
+        method: "PUT",
         body: formData
     });
     window.location.reload();
+}
+
+function imageSize(img) {
+    img.style.width = window.innerWidth / 5 + 'px';
+    img.style.height = window.innerHeight / 4 + 'px';
+}
+
+function figureSize(figure) {
+    for (let i in document.getElementsByTagName("figcaption")) {
+        i.style.width = window.innerWidth / 5 + 'px';
+    }
 }
