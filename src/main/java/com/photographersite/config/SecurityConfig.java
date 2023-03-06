e1abf33f1296b83a81927cd49fd821dc9f0db28f
Hallworld <prishedko.ilya@yandex.ru>
2023-03-06 16:34:42 +0400

    Remove non-existent handler

--- src/main/java/com/photographersite/config/SecurityConfig.java
+++ src/main/java/com/photographersite/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.photographersite.config;
 
-import com.photographersite.handler.CustomAccessDeniedHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,20 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
 
         http.formLogin()
                 .and()
